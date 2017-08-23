427753f737634ff3b248043b6f347697ab85f144
keeradit <keeradit@kitchenup.com>
2017-08-23 13:41:58 +0700

    [react] Created tests for Clock component (part 1)

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,8 +2,20 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var Clock = React.createClass({
+    formatSeconds: function (totalSeconds) {
+        var seconds = totalSeconds % 60;
+        var minute = Math.floor(totalSeconds / 60);
 
-    render: function() {
+        if(minute < 10) {
+            minute = '0' + minute;
+        }
+        if(seconds < 10) {
+            seconds = '0' + seconds;
+        }
+
+        return minute + ':' + seconds;
+    },
+    render: function () {
         return (
             <div />
         );
--- src/tests/components/clock.test.jsx
+++ src/tests/components/clock.test.jsx
@@ -10,4 +10,36 @@ describe('Clock', () => {
     it('should exist', () => {
         expect(Clock).toExist();
     });
+
+    describe('Format Seconds', () => {
+        it('should format seconds', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+        it('should format seconds when minutes is less than 10', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 81;
+            var expected = '01:21';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+        it('should format seconds when seconds is less than 10', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 3001;
+            var expected = '50:01';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+        it('should format seconds when minutes and seconds are less than 10', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+    });
 });
