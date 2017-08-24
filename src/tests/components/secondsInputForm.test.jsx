9137ddd60c52367942d25acdf8941bc8b672ec6f
keeradit <keeradit@kitchenup.com>
2017-08-24 15:52:01 +0700

    [react] Add a test for SecondsInputForm

--- src/tests/components/secondsInputForm.test.jsx
+++ src/tests/components/secondsInputForm.test.jsx
@@ -5,3 +5,22 @@ const $ = require('jQuery');
 const TestUtils = require('react-addons-test-utils');
 
 const SecondsInputForm = require('SecondsInputForm');
+
+describe('SecondsInputForm', () => {
+    it('should exist', () => {
+        expect(SecondsInputForm).toExist();
+    });
+
+    it('should call onSubmitHandler if valid input', () => {
+        var seconds = '109';
+
+        var spy = expect.createSpy();
+        var inputForm = TestUtils.renderIntoDocument(<SecondsInputForm onSubmitHandler={spy}/>);
+        var $element = $(ReactDOM.findDOMNode(inputForm));
+
+        inputForm.refs.seconds.value = seconds;
+        TestUtils.Simulate.submit($element.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(109);
+    });
+})
